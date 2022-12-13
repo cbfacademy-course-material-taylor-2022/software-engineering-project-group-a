9ae92bd2179ef971276d41b589d95af16e791f69
kxiru <nkeirukaw@gmail.com>
2022-12-13 22:32:15 +0000

    Temporary login page input

--- client/src/pages/Homescreen.js
+++ client/src/pages/Homescreen.js
@@ -43,8 +43,16 @@ function HomeScreen() {
             <div>
                 <p>This page will serve as our Welcome / Home screen, but for now; Here is a faux Login button:</p>
                 <p>Clicking the 'login' button below, should take you to the user's signed in, dashboard.</p>
+                    <hr></hr>
+                <p>Username:</p>
+                <input></input>
+                <p>Password:</p>
+                <input></input>
+                <div>
+                    <button><Link to="/dashboard">Login / Go to Dashboard</Link></button>
+                </div>
             </div>
-            <Link to="/dashboard">Login / Go to Dashboard</Link>
+            
         </div>
     )
 }
