8900e5507ab593a395dfccfcd629d25bec053fc6
kxiru <nkeirukaw@gmail.com>
2022-12-13 22:42:05 +0000

    Added comments to help the development of the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,3 +1,14 @@
+// This Header Component should cover the following pages:
+
+// * Home
+// * About
+// * My Dashboard
+// >>> My Account
+// >>> My Expenses
+// >>> My Pots
+
+// The Header / Navigation bar should make sure that it is in line with the UI and design. (Colours and such)
+
 function Header() {
     return(
         <div>
