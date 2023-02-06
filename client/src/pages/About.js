5e40046ca8087cc7017512252ae55b068571aecc
rahelyibrish <rahel.yibrish@gmail.com>
2023-02-06 00:07:30 +0000

    update to dashboard

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -21,23 +21,6 @@ function About() {
         ease. So what are you waiting for? Sign up today and take control of
         your finances with Wollet.
         <br></br>
-        <br></br>
-        Download Wollet app now and start managing your finances.
-      </p>
-      <p>
-        Welcome to Wollet App - your personal finance solution. With our
-        easy-to-use interface, managing your money has never been simpler.
-      </p>
-      <p>
-        Create personalised 'pots' to save for specific goals like a rainy day
-        fund or a house deposit. Track your spending with our financial
-        accountability reports and stay on top of your finances with custom
-        notifications.
-      </p>
-      <p>
-        Our app makes it easy for you to see how much you have in each 'pot' at
-        a glance. Create your financial profile and secure authorisation for
-        added peace of mind.
       </p>
       <p>
         With Wollet, you can set and reach your savings goals with ease. Take
@@ -45,7 +28,6 @@ function About() {
       </p>{" "}
       Download Wollet App and start managing your money the smart way. Sign up
       for free and start creating your personalised 'pots' now.
-      <hr></hr>
     </div>
   );
 }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -14,6 +14,26 @@ import {
 import { Card, Space, Statistic, Table, Typography } from "antd";
 import "../App.css";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function Dashboard(props) {
   return (
     <div>
@@ -43,24 +63,6 @@ function DashboardDisplay(props) {
       </h1>
       <div>
         Welcome to the Wollet Dashboard!<br></br>
-        <p>
-          {" "}
-          Your one-stop shop for tracking, managing, and budgeting your money.
-        </p>
-        <p>
-          Here, you can view your personalised 'pots' and see how much you have
-          saved for each goal. Our app generates financial accountability
-          reports so you can see your progress over time. Create new 'pots' to
-          save for specific goals or edit existing ones. Keep track of your
-          spending by setting spending limits and receiving custom notifications
-          when you reach them. Want to set up a daily, weekly, or monthly report
-          summarising your 'pots'? Simply click on the "Reports" button in the
-          top right corner.
-        </p>
-        <p>
-          Don't forget to check out our personalised profile section, where you
-          can set your saving goals and budgeting plans.
-        </p>
       </div>
       <div className="potDiv">
         <div className="mainPot">
@@ -179,6 +181,7 @@ function DashboardDisplay(props) {
       </Space>
       <Space>
         <RecentOrders />
+        <DashboardChart />
       </Space>
     </div>
   );
@@ -238,6 +241,52 @@ function RecentOrders() {
         dataSource={dataSource}
       ></Table>
     </>
+  );
+}
+
+function DashboardChart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Statistics",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Incoming",
+        data: labels.map(() => Math.random() * 1000),
+        backgroundColor: "rgba(0, 168, 106, 0.5)",
+      },
+      {
+        label: "Outgoing",
+        data: labels.map(() => Math.random() * 1000),
+        backgroundColor: "rgba(132, 156, 147, 0.5)",
+      },
+    ],
+  };
+  return (
+    <Card style={{ width: 500, height: 350 }}>
+      <Bar options={options} data={data} />
+    </Card>
   );
 }
 
